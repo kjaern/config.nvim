e318f9aebf1acf6076adfefbbc7c3c84ca881375
kjaern <kenneth_kjaer_nielsen@hotmail.com>
2024-05-17 22:34:49 +0200

    Migrating from kickstart derived config

---
+++ plugin/options.lua
@@ -0,0 +1,84 @@
+local opt = vim.opt
+
+opt.number = true
+opt.relativenumber = true
+
+-- Enable mouse mode, can be useful for resizing splits for example!
+opt.mouse = 'a'
+
+opt.showmode = false
+
+-- Sync clipboard between OS and Neovim.
+--  Remove this option if you want your OS clipboard to remain independent.
+--  See `:help 'clipboard'`
+opt.clipboard = 'unnamedplus'
+
+-- TODO: What is this?
+opt.breakindent = true
+
+-- Case-insensitive searching UNLESS \C or capital in search
+opt.ignorecase = true
+opt.smartcase = true
+
+-- Keep signcolumn on by default
+opt.signcolumn = 'yes'
+
+-- Decrease update time
+opt.updatetime = 250
+opt.timeoutlen = 300
+
+-- Configure how new splits should be opened
+opt.splitright = true
+opt.splitbelow = true
+
+-- Sets how neovim will display certain whitespace in the editor.
+--  See `:help 'list'`
+--  and `:help 'listchars'`
+opt.list = true
+opt.listchars = { tab = '» ', trail = '·', nbsp = '␣' }
+
+-- Preview substitutions live, as you type!
+opt.inccommand = 'split'
+
+-- Show which line your cursor is on
+opt.cursorline = true
+
+-- Minimal number of screen lines to keep above and below the cursor.
+opt.scrolloff = 10
+
+-- [[ Basic Keymaps ]]
+--  See `:help keymap.set()`
+
+-- Set highlight on search, but clear on pressing <Esc> in normal mode
+opt.hlsearch = true
+
+-- Primagen options
+--
+--
+opt.nu = true
+opt.relativenumber = true
+
+opt.tabstop = 4
+opt.softtabstop = 4
+opt.shiftwidth = 4
+opt.expandtab = true
+
+opt.smartindent = true
+
+opt.wrap = false
+
+opt.swapfile = false
+opt.backup = false
+opt.undodir = vim.fn.expand '~' .. '/.vim/undodir'
+opt.undofile = true
+
+opt.hlsearch = false
+opt.incsearch = true
+
+opt.termguicolors = true
+
+opt.scrolloff = 8
+opt.signcolumn = 'yes'
+--opt.isfname:append '@-@'
+
+opt.updatetime = 50
