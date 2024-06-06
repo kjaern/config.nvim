83c40943e2308614919cd526f483dd78143a4c71
kknielsen <kknielsen@xnovotech.com>
2024-06-06 09:29:57 +0200

    Updated lsp settings /004 󰈙  arr.txt

--- lua/custom/plugins/debug.lua
+++ lua/custom/plugins/debug.lua
@@ -19,7 +19,7 @@ return {
         'jay-babu/mason-nvim-dap.nvim',
 
         -- Add your own debuggers here
-        -- 'leoluz/nvim-dap-go',
+        'leoluz/nvim-dap-go',
         'mfussenegger/nvim-dap-python',
         'ziglang/zig.vim',
     },
@@ -86,7 +86,7 @@ return {
         dap.listeners.before.event_exited['dapui_config'] = dapui.close
 
         -- Install golang specific config
-        -- require('dap-go').setup()
+        require('dap-go').setup()
         -- require('dap-zig').setup()
         require('dap-python').setup '~/pythonNvim/Scripts/python.exe'
 
