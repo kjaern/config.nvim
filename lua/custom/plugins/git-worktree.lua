5b62dfa4d2d1d3741371d554c789423cccb9d66b
kknielsen <kknielsen@xnovotech.com>
2024-06-20 13:50:04 +0200

    worktree indentation

--- lua/custom/plugins/git-worktree.lua
+++ lua/custom/plugins/git-worktree.lua
@@ -1,19 +1,20 @@
 return {
-  'ThePrimeagen/git-worktree.nvim',
-  opts = {},
-  -- Optional dependencies
-  dependencies = { "nvim-tree/nvim-web-devicons" },
+    'ThePrimeagen/git-worktree.nvim',
+    opts = {},
+    -- Optional dependencies
+    dependencies = { "nvim-tree/nvim-web-devicons" },
 
-  config = function()
-    local telescope = require 'telescope'
-    require('git-worktree').setup()
-    -- vim.keymap.set("n", "<leader>gw", "<CMD>require('telescope').extensions.git_worktree.git_worktrees()<CR>",
-    vim.keymap.set('n', '<leader>gw', function()
-      telescope.extensions.git_worktree.git_worktrees()
-    end)
-    vim.keymap.set('n', '<leader>gwc', function()
-      telescope.extensions.git_worktree.create_git_worktree()
-    end)
-  end,
+    config = function()
+        local telescope = require 'telescope'
+        require('git-worktree').setup()
+        -- vim.keymap.set("n", "<leader>gw", "<CMD>require('telescope').extensions.git_worktree.git_worktrees()<CR>",
+        vim.keymap.set('n', '<leader>gw', function(sdf)
+            print(sdf)
+            telescope.extensions.git_worktree.git_worktrees()
+        end)
+        vim.keymap.set('n', '<leader>gwc', function()
+            telescope.extensions.git_worktree.create_git_worktree()
+        end)
+    end,
 
 }
