d63f5b5b75c4a6b6c32929e1a6ad74c384be1be4
kjaern <kenneth_kjaer_nielsen@hotmail.com>
2024-06-29 12:58:27 +0200

    Updated global snippets

--- lua/custom/snippets/global.lua
+++ lua/custom/snippets/global.lua
@@ -7,12 +7,36 @@ local function uuid4()
     local id, _ = vim.fn.system('python -c "import uuid; print(uuid.uuid4())"'):gsub('\n', '')
     return id
 end
+-- ls.add_snippets("all", {
+--     ls.parser.parse_snippet("uuid", uuid4()),
+-- })
 ls.add_snippets("all", {
-    s({
+    ls.snippet({
         trig = 'uuid',
         name = 'UUID',
         dscr = 'Generate a unique UUID'
     }, {
-        d(1, function() return sn(nil, i(1, uuid4())) end)
+        ls.function_node(uuid4)
+        -- ls.dynamic_node(1, function() return ls.snippet_node(nil, ls.insert_node(1, uuid4())) end)
+        -- ls.dynamic_node(1, function() return ls.snippet_node(nil, uuid4()) end)
+    }),
+})
+
+local function getDate()
+    local id, _ = vim.fn.system(
+            [===[python -c "from datetime import datetime; print(datetime.now().strftime('%Y/%m/%d'))"]===])
+        :gsub(
+            '\n', '')
+    return id
+end
+
+ls.add_snippets("all", {
+    s({
+        trig = 'date',
+        name = 'Date',
+        dscr = 'Generate current date string'
+    }, {
+        -- ls.dynamic_node(1, function() return ls.snippet_node(nil, ls.insert_node(1, getDate())) end),
+        ls.function_node(getDate)
     }),
 })
