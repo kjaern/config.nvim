2bc5942fa83474b42176d820084b2159d890e5a4
kjaern <kenneth_kjaer_nielsen@hotmail.com>
2024-06-30 10:43:36 +0200

    Added resize remap

--- plugin/remap.lua
+++ plugin/remap.lua
@@ -82,6 +82,9 @@ vim.keymap.set('n', '<C-l>', '<C-w><C-l>', { desc = 'Move focus to the right win
 vim.keymap.set('n', '<C-j>', '<C-w><C-j>', { desc = 'Move focus to the lower window' })
 vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper window' })
 
+vim.keymap.set('n', '<A-l>', '<C-w>>', { desc = 'Increase to right' })
+vim.keymap.set('n', '<A-h>', '<C-w><', { desc = 'Increase to left' })
+
 -- [[ Basic Autocommands ]]
 --  See `:help lua-guide-autocommands`
 
