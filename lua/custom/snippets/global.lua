419ea5ed55ef54deca8be258d9c199978a8d1888
kjaern <kenneth_kjaer_nielsen@hotmail.com>
2024-07-04 07:17:41 +0200

    Removed custom global snippets, as they are now redundant

--- lua/custom/snippets/global.lua
+++ lua/custom/snippets/global.lua
@@ -1,41 +1,41 @@
-local ls = require "luasnip"
-
--- local s, sn = ls.snippet, ls.snippet_node
--- local i, d = ls.insert_node, ls.dynamic_node
-
-local fmt = require("luasnip.extras.fmt").fmt
-
-local function uuid4()
-    local id, _ = vim.fn.system('python -c "import uuid; print(uuid.uuid4())"'):gsub('\n', '')
-    return id
-end
+-- local ls = require "luasnip"
+--
+-- -- local s, sn = ls.snippet, ls.snippet_node
+-- -- local i, d = ls.insert_node, ls.dynamic_node
+--
+-- local fmt = require("luasnip.extras.fmt").fmt
+--
+-- local function uuid4()
+--     local id, _ = vim.fn.system('python -c "import uuid; print(uuid.uuid4())"'):gsub('\n', '')
+--     return id
+-- end
+-- -- ls.add_snippets("all", {
+-- --     ls.parser.parse_snippet("uuid", uuid4()),
+-- -- })
 -- ls.add_snippets("all", {
---     ls.parser.parse_snippet("uuid", uuid4()),
+--     ls.snippet({
+--         trig = 'uuid',
+--         name = 'UUID',
+--         dscr = 'Generate a unique UUID'
+--     }, {
+--         ls.function_node(uuid4)
+--         -- ls.dynamic_node(1, function() return ls.snippet_node(nil, ls.insert_node(1, uuid4())) end)
+--         -- ls.dynamic_node(1, function() return ls.snippet_node(nil, uuid4()) end)
+--     }),
 -- })
-ls.add_snippets("all", {
-    ls.snippet({
-        trig = 'uuid',
-        name = 'UUID',
-        dscr = 'Generate a unique UUID'
-    }, {
-        ls.function_node(uuid4)
-        -- ls.dynamic_node(1, function() return ls.snippet_node(nil, ls.insert_node(1, uuid4())) end)
-        -- ls.dynamic_node(1, function() return ls.snippet_node(nil, uuid4()) end)
-    }),
-})
-
-local function getDate()
-    local id, _ = vim.fn.system(
-            [===[python -c "from datetime import datetime; print(datetime.now().strftime('%Y/%m/%d'))"]===])
-        :gsub(
-            '\n', '')
-    return id
-end
-
-ls.add_snippets("all", {
-    ls.snippet({
-        trig = 'date',
-        name = 'Date',
-        dscr = 'Generate current date string',
-    }, fmt("{}", { vim.fn.strftime("%a %d %b %Y") })),
-})
+--
+-- local function getDate()
+--     local id, _ = vim.fn.system(
+--             [===[python -c "from datetime import datetime; print(datetime.now().strftime('%Y/%m/%d'))"]===])
+--         :gsub(
+--             '\n', '')
+--     return id
+-- end
+--
+-- ls.add_snippets("all", {
+--     ls.snippet({
+--         trig = 'date',
+--         name = 'Date',
+--         dscr = 'Generate current date string',
+--     }, fmt("{}", { vim.fn.strftime("%a %d %b %Y") })),
+-- })
