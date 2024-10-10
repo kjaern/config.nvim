78033629636cb16611aa36716f7d6002ffbb36d2
kjaern <kenneth_kjaer_nielsen@hotmail.com>
2024-10-10 05:19:48 +0200

    updated init.lua, as lazy does it

--- init.lua
+++ init.lua
@@ -8,11 +8,21 @@ if vim.fn.exists('g:os') == 0 then
     end
 end
 
-local lazypath = vim.fn.stdpath 'data' .. '/lazy/lazy.nvim'
-if not vim.loop.fs_stat(lazypath) then
-    local lazyrepo = 'https://github.com/folke/lazy.nvim.git'
-    vim.fn.system { 'git', 'clone', '--filter=blob:none', '--branch=stable', lazyrepo, lazypath }
-end ---@diagnostic disable-next-line: undefined-field
+-- Bootstrap lazy.nvim
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not (vim.uv or vim.loop).fs_stat(lazypath) then
+    local lazyrepo = "https://github.com/folke/lazy.nvim.git"
+    local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
+    if vim.v.shell_error ~= 0 then
+        vim.api.nvim_echo({
+            { "Failed to clone lazy.nvim:\n", "ErrorMsg" },
+            { out,                            "WarningMsg" },
+            { "\nPress any key to exit..." },
+        }, true, {})
+        vim.fn.getchar()
+        os.exit(1)
+    end
+end
 vim.opt.rtp:prepend(lazypath)
 
 require("lazy").setup({ import = "custom/plugins" }, {
--- lua/custom/plugins/blender.lua
+++ lua/custom/plugins/blender.lua
@@ -1,13 +1,13 @@
 return {
--- :Blender - Open the Blender.nvim UI
--- :BlenderLaunch - Launch a Blender profile
--- :BlenderManage - Manage a running Blender task
--- :BlenderReload - Reload the Blender add-on
--- :BlenderWatch - Watch for changes and reload the add-on
--- :BlenderUnwatch - Stop watching for changes
+    -- :Blender - Open the Blender.nvim UI
+    -- :BlenderLaunch - Launch a Blender profile
+    -- :BlenderManage - Manage a running Blender task
+    -- :BlenderReload - Reload the Blender add-on
+    -- :BlenderWatch - Watch for changes and reload the add-on
+    -- :BlenderUnwatch - Stop watching for changes
     "b0o/blender.nvim",
     config = function()
-        require("blender").setup()
+        require("blender").setup {}
     end,
     dependencies = {
         "MunifTanjim/nui.nvim",
