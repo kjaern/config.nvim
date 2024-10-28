be24a40c4be2711d81d399cac662133fe981e6eb
kjaern <kenneth_kjaer_nielsen@hotmail.com>
2024-10-28 11:08:16 +0100

    Added clangd and simplified python snippets

--- lua/custom/snippets/python.lua
+++ lua/custom/snippets/python.lua
@@ -1,14 +1,10 @@
 require("luasnip.session.snippet_collection").clear_snippets "python"
-print("python")
 local ls = require "luasnip"
-
-local s, sn = ls.snippet, ls.snippet_node
-local t, i, d = ls.text_node, ls.insert_node, ls.dynamic_node
 
 local fmt = require("luasnip.extras.fmt").fmt
 
 ls.add_snippets("python", {
-    s("xn", fmt([[
+    ls.snippet("xn", fmt([[
     #
     # This software is(c) Xnovo Technology ApS {}. All use is subject to
     # separate license agreement.
