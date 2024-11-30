aad29e81a4e5508c8a2b872079554d69870ec73d
kjaern <kenneth_kjaer_nielsen@hotmail.com>
2024-11-30 16:11:52 +0100

    updated venv to my own fork

--- lua/custom/plugins/python_venv.lua
+++ lua/custom/plugins/python_venv.lua
@@ -1,31 +1,52 @@
 return {
+    -- dir = "C:\\Users\\Kenneth\\Development\\venv-selector.nvim",
+    -- config = true,
 
-    "linux-cultist/venv-selector.nvim",
+    -- "linux-cultist/venv-selector.nvim",
+    "kjaern/venv-selector.nvim",
+    branch = "regexp", -- This is the regexp branch, use this for the new version
     dependencies = {
         "neovim/nvim-lspconfig",
         "mfussenegger/nvim-dap", "mfussenegger/nvim-dap-python", --optional
         { "nvim-telescope/telescope.nvim", branch = "0.1.x", dependencies = { "nvim-lua/plenary.nvim" } },
     },
     lazy = false,
-    branch = "regexp", -- This is the regexp branch, use this for the new version
-    config = function()
-        require("venv-selector").setup {
-            options = {
-                -- on_venv_activate_callback = nil,           -- callback function for after a venv activates
-                -- enable_default_searches = true,            -- switches all default searches on/off
-                -- enable_cached_venvs = true,               -- use cached venvs that are activated automatically when a python file is registered with the LSP.
-                cached_venv_automatic_activation = false, -- if set to false, the VenvSelectCached command becomes available to manually activate them.venv
-                -- activate_venv_in_terminal = true,          -- activate the selected python interpreter in terminal windows opened from neovim
-                -- set_environment_variables = true,          -- sets VIRTUAL_ENV or CONDA_PREFIX environment variables
-                -- notify_user_on_venv_activation = false,    -- notifies user on activation of the virtual env
-                -- search_timeout = 5,                        -- if a search takes longer than this many seconds, stop it and alert the user
-                -- debug = false,                             -- enables you to run the VenvSelectLog command to view debug logs
-                -- fd_binary_name = M.find_fd_command_name(), -- plugin looks for `fd` or `fdfind` but you can set something else here
 
-                -- telescope viewer options
-                -- on_telescope_result_callback = nil, -- callback function for modifying telescope results
-                -- show_telescope_search_type = true,  -- shows which of the searches found which venv in telescope
-                -- telescope_filter_type = "substring" -- when you type something in telescope, filter by "substring" or "character"
+    config = function()
+        local function shorter_name(filename)
+            print("filename")
+            print(filename)
+            return filename
+
+            -- return filename:gsub(os.getenv("HOME"), "~"):gsub("/bin/python", "")
+        end
+
+
+        require("venv-selector").setup {
+            settings = {
+                options = {
+                    -- on_venv_activate_callback = nil,           -- callback function for after a venv activates
+                    -- enable_default_searches = true,            -- switches all default searches on/off
+                    -- enable_cached_venvs = true,               -- use cached venvs that are activated automatically when a python file is registered with the LSP.
+                    cached_venv_automatic_activation = false, -- if set to false, the VenvSelectCached command becomes available to manually activate them.venv
+                    -- activate_venv_in_terminal = true,          -- activate the selected python interpreter in terminal windows opened from neovim
+                    -- set_environment_variables = true,          -- sets VIRTUAL_ENV or CONDA_PREFIX environment variables
+                    notify_user_on_venv_activation = true, -- notifies user on activation of the virtual env
+                    -- search_timeout = 5,                        -- if a search takes longer than this many seconds, stop it and alert the user
+                    debug = true,                          -- enables you to run the VenvSelectLog command to view debug logtrues
+                    -- fd_binary_name = M.find_fd_command_name(), -- plugin looks for `fd` or `fdfind` but you can set something else here
+
+                    -- telescope viewer options
+                    on_telescope_result_callback = shorter_name, -- callback function for modifying telescope results
+                    show_telescope_search_type = true,           -- shows which of the searches found which venv in telescope
+                    -- telescope_filter_type = "substring" -- when you type something in telescope, filter by "substring" or "character"
+                },
+                search = {
+                    anaconda_base = {
+                        command = "fd python.exe$ --full-path -IH -a --color never",
+                        type = "anaconda",
+                    },
+                },
             },
         }
     end,
@@ -38,24 +59,28 @@ return {
         { '<leader>vv', function()
             print("Python env: " .. require("venv-selector").venv())
         end },
+        { '<leader>vvv', function()
+            print("Python env: " .. require("venv-selector").venv())
+        end },
     },
     -- 'linux-cultist/venv-selector.nvim',
     -- -- 'kjaern/venv-selector.nvim',
     -- dependencies = { 'neovim/nvim-lspconfig', 'nvim-telescope/telescope.nvim', 'mfussenegger/nvim-dap-python' },
-    opts = {
-        --     -- Your options go here
-        name = { 'python', "venv", ".venv", 'conda' },
-        --     -- auto_refresh = false
-        --     parents = 0,
-        --     anaconda = {
-        --         python_executable = 'python.exe',
-        --         python_parent_dir = 'Scripts/',
-        --         -- python_parent_dir = '.',
-        --         -- dap_enabled = true,
-        --         -- enable_debug_output = true,
-        --     },
-        python_parent_dir = 'Scripts/',
-    },
+    --
+    -- opts = {
+    --     --     -- Your options go here
+    --     name = { 'python', "venv", ".venv", "conda", ".conda" },
+    --     --     -- auto_refresh = false
+    --     --     parents = 0,
+    --     anaconda = {
+    --         python_executable = '.conda/python.exe',
+    --         -- python_parent_dir = 'Scripts/',
+    --         python_parent_dir = '.conda',
+    --         -- dap_enabled = true,
+    --         -- enable_debug_output = true,
+    --     },
+    --     python_parent_dir = 'Scripts/',
+    -- },
     -- event = 'VeryLazy', -- Optional: needed only if you want to type `:VenvSelect` without a keymapping
     -- keys = {
     --     -- Keymap to open VenvSelector to pick a venv.
--- plugin/pythonAppEnv.lua
+++ plugin/pythonAppEnv.lua
@@ -4,7 +4,7 @@ local conf = require('telescope.config').values
 local actions = require 'telescope.actions'
 local actionState = require 'telescope.actions.state'
 
-local function createEnv(envName)
+local function createEnv(envName, env)
     vim.cmd 'below split'
     vim.cmd 'terminal'
     vim.fn.feedkeys 'a'
@@ -12,13 +12,13 @@ local function createEnv(envName)
     vim.fn.feedkeys('clear' .. enter)
     vim.fn.feedkeys("C:\\Users\\kknielsen\\AppData\\Local\\miniconda3\\Scripts\\activate.bat" .. enter)
     -- vim.fn.feedkeys('~/pythonNvim/Scripts/python.exe' .. enter)
-    vim.fn.feedkeys('createDevEnv python ' .. envName .. enter)
+    vim.fn.feedkeys('createDevEnv ' .. ' ' .. env .. ' ' .. envName .. enter)
 
     vim.fn.feedkeys("C:\\Users\\kknielsen\\AppData\\Local\\miniconda3\\Scripts\\deactivate.bat" .. enter)
     -- vim.fn.feedkeys('conda deactivate' .. envName .. enter)
 end
 
-local createAppEnv = function(opts)
+local createAppEnv = function(opts, env)
     opts = opts or {}
 
     local cwDir = vim.fn.getcwd()
@@ -27,7 +27,7 @@ local createAppEnv = function(opts)
     for key, n in pairs(cwdContent) do
         cwdContent[key] = 'apps/' .. n
     end
-    print(cwdContent)
+    print(env)
     pickers
         .new(opts, {
 
@@ -44,7 +44,7 @@ local createAppEnv = function(opts)
                     if selection[1] == vim.fn.expand '%' then
                         print 'None selected'
                     else
-                        createEnv(selection[1])
+                        createEnv(selection[1], env)
                         print('selected ' .. selection[1])
                         vim.print(cwdContent)
                     end
@@ -55,4 +55,83 @@ local createAppEnv = function(opts)
         :find()
 end
 
-vim.keymap.set('n', '<leader>ka', createAppEnv)
+local decorateCreateEnv = function(env)
+    return function(opts)
+        createAppEnv(opts, env)
+    end
+end
+
+vim.keymap.set('n', '<leader>kp', decorateCreateEnv('python'))
+vim.keymap.set('n', '<leader>ka', decorateCreateEnv('conda'))
+
+local Job = require 'plenary.job'
+local createWorktree = function(opts)
+    opts = opts or {}
+    -- local return_val = 'test'
+
+    local cwd = vim.loop.cwd()
+
+    local stdout, code = Job:new({
+        'git',
+        'status',
+        cwd = cwd,
+    }):sync() -- or start()
+
+    if code ~= 0 then
+        print('failed')
+        return
+    else
+        for k, v in pairs(stdout) do
+            print(v)
+        end
+        stdout = table.concat(stdout, "\n")
+        print(stdout)
+    end
+
+
+    print("godby\n goodby")
+
+    -- print(return_val)
+    -- print("hello?")
+
+    -- vim.cmd 'below split'
+    -- vim.cmd 'terminal'
+    -- vim.fn.feedkeys 'a'
+    -- local enter = vim.api.nvim_replace_termcodes('<CR>', true, true, true)
+    -- vim.fn.feedkeys('clear' .. enter)
+    -- vim.fn.feedkeys("git worktree list" .. enter)
+    --
+    -- local cwDir = vim.fn.getcwd()
+    -- --local cwdContent = vim.split(vim.fn.glob(cwDir .. '/apps/*'), '\n', { trimempty = true })
+    -- local cwdContent = vim.fn.readdir(cwDir .. '/apps')
+    -- for key, n in pairs(cwdContent) do
+    --     cwdContent[key] = 'apps/' .. n
+    -- end
+    -- pickers
+    --     .new(opts, {
+    --
+    --         finder = finders.new_table {
+    --             results = cwdContent,
+    --             --{ 'blue', 'sdfasd' },
+    --         },
+    --         sorter = conf.generic_sorter(opts),
+    --         attach_mappings = function(bufnr, map)
+    --             actions.select_default:replace(function()
+    --                 actions.close(bufnr)
+    --                 local selection = actionState.get_selected_entry()
+    --
+    --                 if selection[1] == vim.fn.expand '%' then
+    --                     print 'None selected'
+    --                 else
+    --                     createEnv(selection[1], env)
+    --                     print('selected ' .. selection[1])
+    --                     vim.print(cwdContent)
+    --                 end
+    --             end)
+    --             return true
+    --         end,
+    --     })
+    --     :find()
+end
+
+vim.keymap.set('n', '<leader>kw', createWorktree)
