fa81ac066730296cef7929268f6acc26ddc80d3d
kjaern <kenneth_kjaer_nielsen@hotmail.com>
2025-10-05 01:28:21 +0200

    Added templates for plugins

--- lua/optionalPluginsDefaults.lua
+++ lua/optionalPluginsDefaults.lua
@@ -1,4 +1,11 @@
 return {
-    { import = "custom/plugins" },
-    -- { import = "custom/optionalPlugins/blender" },
+	-- { import = "custom/optionalPlugins/blender" },
+	-- { import = "custom/optionalPlugins/clangd-extensios" },
+	-- { import = "custom/optionalPlugins/filetree" },
+	-- { import = "custom/optionalPlugins/git-worktree" },
+	-- { import = "custom/optionalPlugins/markdown" },
+	-- { import = "custom/optionalPlugins/python_venv" },
+	-- { import = "custom/optionalPlugins/todo" },
+	-- { import = "custom/optionalPlugins/vimbegood" },
+	-- { import = "custom/optionalPlugins/zig" },
 }
