b63978a3c20ef66f0c80ed7465b13207d6b66caa
kjaern <kenneth_kjaer_nielsen@hotmail.com>
2025-11-22 09:20:55 +0100

    Finally got venv select to work porperly

--- lua/custom/optionalPlugins/python_venv.lua
+++ lua/custom/optionalPlugins/python_venv.lua
@@ -13,47 +13,40 @@ return {
 	lazy = false,
 
 	config = function()
-		local function shorter_name(filename)
-			print("filename")
-			print(filename)
-			return filename
-
-			-- return filename:gsub(os.getenv("HOME"), "~"):gsub("/bin/python", "")
+		local function shorter_name(filename, source)
+			return "." .. string.gsub(filename, vim.loop.cwd(), "")
 		end
 
 		require("venv-selector").setup({
+			options = { debug = true },
 			settings = {
 				options = {
 					-- on_venv_activate_callback = nil,           -- callback function for after a venv activates
 					-- enable_default_searches = true,            -- switches all default searches on/off
 					enable_cached_venvs = true, -- use cached venvs that are activated automatically when a python file is registered with the LSP.
 					cached_venv_automatic_activation = false, -- if set to false, the VenvSelectCached command becomes available to manually activate them.venv
-					-- activate_venv_in_terminal = true,          -- activate the selected python interpreter in terminal windows opened from neovim
-					-- set_environment_variables = true,          -- sets VIRTUAL_ENV or CONDA_PREFIX environment variables
-					notify_user_on_venv_activation = true, -- notifies user on activation of the virtual env
+					activate_venv_in_terminal = true, -- activate the selected python interpreter in terminal windows opened from neovim
+					-- set_environment_variables = true, -- sets VIRTUAL_ENV or CONDA_PREFIX environment variables
+					-- notify_user_on_venv_activation = true, -- notifies user on activation of the virtual env
 					-- search_timeout = 5,                        -- if a search takes longer than this many seconds, stop it and alert the user
 					debug = true, -- enables you to run the VenvSelectLog command to view debug logtrues
 					-- fd_binary_name = M.find_fd_command_name(), -- plugin looks for `fd` or `fdfind` but you can set something else here
 
-					-- telescope viewer options
-					on_telescope_result_callback = shorter_name, -- callback function for modifying telescope results
 					show_telescope_search_type = true, -- shows which of the searches found which venv in telescope
 					-- telescope_filter_type = "substring" -- when you type something in telescope, filter by "substring" or "character"
 				},
 				search = {
-					anaconda_base = {
-						command = "fd python$ --full-path -IH -a --color never",
+					conda = {
+						command = "fd conda//python.exe$ --full-path -IH -a --color never",
 						type = "anaconda",
+						on_telescope_result_callback = shorter_name, -- callback function for modifying telescope results
 					},
 				},
 			},
 		})
 	end,
 	keys = {
-		-- { ",v", "<cmd>VenvSelect<cr>" },
-		-- { '<leader>vs', '<cmd>VenvSelect<cr>',      { show_telescope_search_type = true, file_ignore_patterns = { ".venv" } },
 		{ "<leader>vs", "<cmd>VenvSelect<cr>", { show_telescope_search_type = true } },
-		-- Keymap to retrieve the venv from a cache (the one previously used for the same project directory).
 		{ "<leader>vc", "<cmd>VenvSelectCached<cr>" },
 		{
 			"<leader>vv",
